41ec65894c06579f3b910cd220c95c212affce13
Asifabc555 <107749289+Asifabc555@users.noreply.github.com>
2022-09-22 12:02:34 +0530

    Update welcome.py

--- AsukaRobot/modules/welcome.py
+++ AsukaRobot/modules/welcome.py
@@ -252,16 +252,16 @@ def new_member(update: Update, context: CallbackContext):
                     reply_to_message_id=reply,
                 parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
-                [
-                  [                  
+                
+                             
                        
                             
                             
                        
                            
                             
-                     ],
-                     [                  
+                     
+                                    
                      
                            
                             
