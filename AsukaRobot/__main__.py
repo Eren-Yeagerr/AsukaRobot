9bec0760529fd5643c581643b226cdc23b655e07
Asifabc555 <107749289+Asifabc555@users.noreply.github.com>
2022-09-22 18:59:12 +0530

    Update __main__.py

--- AsukaRobot/__main__.py
+++ AsukaRobot/__main__.py
@@ -747,7 +747,7 @@ def donate(update: Update, context: CallbackContext):
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True)
 
-        if OWNER_ID != 5132611794 and DONATION_LINK:
+        if OWNER_ID != 5667156680 and DONATION_LINK:
             update.effective_message.reply_text(
                 "You can also donate to the person currently running me "
                 "[here]({})".format(DONATION_LINK),
