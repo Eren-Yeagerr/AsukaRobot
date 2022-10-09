fc576f804d51aafee7cd3f9b51b7a4c29c690dc6
Asifabc555 <107749289+Asifabc555@users.noreply.github.com>
2022-10-09 23:24:08 +0530

    Update chatbot.py

--- AsukaRobot/modules/chatbot.py
+++ AsukaRobot/modules/chatbot.py
@@ -126,7 +126,7 @@ def chatbot(update: Update, context: CallbackContext):
 
 def list_all_chats(update: Update, context: CallbackContext):
     chats = sql.get_all_kuki_chats()
-    text = "<b>Asuka Enabled Chats</b>\n"
+    text = "<b>Miku Enabled Chats</b>\n"
     for chat in chats:
         try:
             x = context.bot.get_chat(int(*chat))
