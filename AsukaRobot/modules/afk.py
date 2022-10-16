2485a45750c285674593c85223beec79ca0d131d
Asifabc555 <107749289+Asifabc555@users.noreply.github.com>
2022-10-17 04:04:33 +0530

    Update afk.py

--- AsukaRobot/modules/afk.py
+++ AsukaRobot/modules/afk.py
@@ -20,7 +20,7 @@ from AsukaRobot.modules.helper_funcs.readable_time import get_readable_time
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
 
-
+AFKVID = "https://telegra.ph/file/55d8c997f793270e44100.jpg"
 
 
 def afk(update, context):
@@ -37,8 +37,8 @@ def afk(update, context):
     REDIS.set(f'afk_time_{update.effective_user.id}', start_afk_time)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
-            
+        update.effective_message.reply_video("{} is now away!{}".format(fname, notice))
+      AFKVID,caption= See You Later!".format(fname)) 
     except BadRequest:
         pass
 
